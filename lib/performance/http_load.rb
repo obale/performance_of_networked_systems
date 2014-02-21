135ce41e0beb7d5590162cbc1f5d3b350cea5f08
Alex Oberhauser <alex.oberhauser@networld.to>
2014-02-21 13:17:00 +0100

    Catching exception.

--- lib/performance/http_load.rb
+++ lib/performance/http_load.rb
@@ -12,32 +12,36 @@ class Performance::HttpLoad
       puts "[ENDPOINT] Measuring for '#{host}'. Current date and time: #{Time.now}"
       url_list = self.write_url_list_file(host)
       rates.length.times do |i|
-        puts "[PERF] Executing: http_load -rate #{rates[i]} -seconds #{seconds[i]} #{url_list}"
-        output = `#{http_load_exec} -rate #{rates[i]} -seconds #{seconds[i]} #{url_list}`
-        perfs = output.lines[4].split(" ")
-        mean = perfs[1]
-        max = perfs[3]
-        min = perfs[5]
+        begin
+          puts "[PERF] Executing: http_load -rate #{rates[i]} -seconds #{seconds[i]} #{url_list}"
+          output = `#{http_load_exec} -rate #{rates[i]} -seconds #{seconds[i]} #{url_list}`
+          perfs = output.lines[4].split(" ")
+          mean = perfs[1]
+          max = perfs[3]
+          min = perfs[5]
 
-        first_line = output.lines[0].split(" ")
-        fetches = first_line[0]
-        max_parallel = first_line[2]
-        fetches_per_second = output.lines[2].split(" ").first
+          first_line = output.lines[0].split(" ")
+          fetches = first_line[0]
+          max_parallel = first_line[2]
+          fetches_per_second = output.lines[2].split(" ").first
 
-        puts output
+          puts output
 
-        Performance::Measurement.create(
-          endpoint: host,
-          mean_msecs: perfs[1],
-          max_msecs: perfs[3],
-          min_msecs: perfs[5],
-          seconds: seconds[i],
-          rate: rates[i],
-          fetches: fetches,
-          max_parallel: max_parallel,
-          fetches_per_second: fetches_per_second
-        )
-        puts Performance::Measurement.last.to_json
+          Performance::Measurement.create(
+            endpoint: host,
+            mean_msecs: perfs[1],
+            max_msecs: perfs[3],
+            min_msecs: perfs[5],
+            seconds: seconds[i],
+            rate: rates[i],
+            fetches: fetches,
+            max_parallel: max_parallel,
+            fetches_per_second: fetches_per_second
+          )
+          puts Performance::Measurement.last.to_json
+        rescue => e
+          puts "[ERROR] Not able to store performance measurements, through exception: #{e}"
+        end
       end
       puts "-----------------"
     end
